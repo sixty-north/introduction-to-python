82718aeb67d2a30e36dcf55f8d277d8eb8bb27c5
Austin Bingham <austin.bingham@gmail.com>
2018-05-08 12:30:41 -0500

    Removed docstrings from fractal.
    This way it'll fit more easily on my tablet's screen.

--- cold-start/fractal.py
+++ cold-start/fractal.py
@@ -4,17 +4,6 @@ import math
 
 
 def mandel(real, imag):
-    """The logarithm of number of iterations needed to
-    determine whether a complex point is in the
-    Mandelbrot set.
-
-    Args:
-        real: The real coordinate
-        imag: The imaginary coordinate
-
-    Returns:
-        An integer in the range 1-255.
-    """
     x = 0
     y = 0
     for i in range(1, 257):
@@ -27,15 +16,6 @@ def mandel(real, imag):
 
 
 def mandelbrot(size_x, size_y):
-    """Make an Mandelbrot set image.
-
-    Args:
-        size_x: Image width
-        size_y: Image height
-
-    Returns:
-        A list of lists of integers in the range 0-255.
-    """
     return [[mandel((3.5 * x / size_x) - 2.5,
                     (2.0 * y / size_y) - 1.0)
              for x in range(size_x)]
