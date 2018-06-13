babfe5f9804909b775f91232fae319a31408e934
Austin Bingham <austin.bingham@gmail.com>
2018-06-13 09:07:26 +0200

    Fixed stupid bug that was through all code examples.

--- v04/zorkalike/zorkalike/rooms/room.py
+++ v04/zorkalike/zorkalike/rooms/room.py
@@ -5,7 +5,7 @@ class Room(ABC):
     def __init__(self,
                  contents=None):
         self.doors = {}
-        self.contents = dict(contents) if contents else None
+        self.contents = dict(contents) if contents else {}
 
     def process_command(self, command, player):
         """Process room-specific commands.
