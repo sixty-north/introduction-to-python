18682fb38e1241e5187828b288908d7e4c667640
Austin Bingham <austin.bingham@gmail.com>
2018-06-15 10:38:23 +0200

    Fixed tests after removal of static room.

--- v03/test/test_connect.py
+++ v03/test/test_connect.py
@@ -3,14 +3,14 @@ import hypothesis.strategies as ST
 import pytest
 
 from zorkalike.direction import Direction
-from zorkalike.rooms.static_room import StaticRoom
+from zorkalike.rooms.room import Room
 from zorkalike.rooms.util import connect, DIR_COMPLEMENTS
 
 
 @given(ST.sampled_from(Direction))
 def test_room_connections_are_complementary(direction):
-    r1 = StaticRoom('Room 1')
-    r2 = StaticRoom('Room 2')
+    r1 = Room()
+    r2 = Room()
     dir_comp = DIR_COMPLEMENTS[direction]
 
     connect(r1, r2, direction)
@@ -20,21 +20,21 @@ def test_room_connections_are_complementary(direction):
 
 @given(ST.sampled_from(Direction))
 def test_connect_throws_ValueError_if_from_door_is_occupied(direction):
-    r1 = StaticRoom('R1')
-    r2 = StaticRoom('R2')
+    r1 = Room()
+    r2 = Room()
     connect(r1, r2, direction)
 
-    r3 = StaticRoom('R3')
+    r3 = Room()
     with pytest.raises(ValueError):
         connect(r1, r3, direction)
 
 
 @given(ST.sampled_from(Direction))
 def test_connect_throws_ValueError_if_to_door_is_occupied(direction):
-    r1 = StaticRoom('R1')
-    r2 = StaticRoom('R2')
+    r1 = Room()
+    r2 = Room()
     connect(r1, r2, direction)
 
-    r3 = StaticRoom('R3')
+    r3 = Room()
     with pytest.raises(ValueError):
         connect(r3, r2, direction)
