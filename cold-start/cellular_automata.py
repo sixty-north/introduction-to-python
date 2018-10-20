285a6f08654cf7f59cfce1170afe13dc0f0284ef
Austin Bingham <austin.bingham@gmail.com>
2018-10-20 09:14:54 +0200

    Simpler expression of cellular-automata example.

--- cold-start/cellular_automata.py
+++ cold-start/cellular_automata.py
@@ -1,37 +1,18 @@
-RULE_30 = {
-    '111': 0,
-    '110': 0,
-    '101': 0,
-    '100': 1,
-    '011': 1,
-    '010': 1,
-    '001': 1,
-    '000': 0,
-}
-
-RULE_110 = {
-    '111': 0,
-    '110': 1,
-    '101': 1,
-    '100': 0,
-    '011': 1,
-    '010': 1,
-    '001': 1,
-    '000': 0,
-
-}
+RULE_30 = [0, 1, 1, 1, 1, 0, 0, 0]
+RULE_110 = [0, 1, 1, 1, 0, 1, 1, 0]
 
 
 def apply(rule, row):
     new_row = [0] * len(row)
-    for i in range(1, len(row) - 1):
-        new_row[i] = rule[''.join(map(str, row[i-1:i+2]))]
+    triples = zip(row, row[1:], row[2:])
+    for index, (a, b, c) in enumerate(triples):
+        rule_index = (a << 2) + (b << 1) + c
+        new_row[index + 1] = rule[rule_index]
     return new_row
 
 
 def generate_cells(rule, num_rows, num_columns):
     row = [0] * num_columns
-    row[num_columns // 2] = 1
     for i in range(1, num_rows):
         yield row
         row = apply(rule, row)
